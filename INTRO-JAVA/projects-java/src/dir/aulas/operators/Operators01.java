4e77ec78d4b3a973c0c4595a4a929e1a9f86a180
ffernanda85 <f.fernanda85@gmail.com>
2023-08-28 08:57:46 -0300

    feat: operators class correction

--- INTRO-JAVA/projects-java/src/dir/aulas/operators/Operators01.java
+++ INTRO-JAVA/projects-java/src/dir/aulas/operators/Operators01.java
@@ -1,25 +1,27 @@
 package dir.aulas.operators;
-public class Operators {
+public class Operators01 {
     public static void main(String[] args) {
         //classe Operadores.java
         String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
                 
         //qual o resultado das expressoes abaixo?
-        String concatenacao ="?"; 
+        String concatenacao = "?";
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+1+1+"1"; //31
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+"1"+1+1; //1111
+        System.out.println(concatenacao);
+        
+        concatenacao = 1+"1"+1+"1"; //1111
+        System.out.println(concatenacao);
+        
+        concatenacao = "1"+1+1+1; //1111
+        System.out.println(concatenacao);
 
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
-        
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
-        
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
-        
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
+        concatenacao = "1" + (1 + 1 + 1); //13 
         System.out.println(concatenacao);
     }
 }
