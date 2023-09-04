84bb5a9514f2d0b406b073087ff50361fc5de170
ffernanda85 <f.fernanda85@gmail.com>
2023-09-04 17:27:02 -0300

    feat: for

---
+++ 03_ESTRUTURAS-DE-REPETICAO-EM-JAVA/example-for/src/ExampleFor.java
@@ -0,0 +1,8 @@
+public class ExampleFor {
+    public static void main(String[] args) throws Exception {
+        for (int sheep = 1; sheep <= 20; sheep++) {
+            System.out.println("Contando carneirinhos: " + sheep);
+        }
+        System.out.println("Joãozinho Dormiu!");
+    }
+}
