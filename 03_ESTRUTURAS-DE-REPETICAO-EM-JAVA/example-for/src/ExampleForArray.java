6f646514d5771b9d53a9e970eba0af02691d449a
ffernanda85 <f.fernanda85@gmail.com>
2023-09-04 17:42:56 -0300

    feat: for each

---
+++ 03_ESTRUTURAS-DE-REPETICAO-EM-JAVA/example-for/src/ExampleForArray.java
@@ -0,0 +1,9 @@
+public class ExampleForArray {
+    public static void main(String[] args) {
+        String students[] = { "FLÁVIA", "BARTOLOMEU", "REGIANE" };
+
+        for (int x = 0; x < students.length; x++) {
+            System.out.println("O Aluno no índice x = " + x + " é " + students[x]);
+        }
+    }
+}
---
+++ 03_ESTRUTURAS-DE-REPETICAO-EM-JAVA/example-for/src/ExampleForEach.java
@@ -0,0 +1,10 @@
+public class ExampleForEach {
+    public static void main(String[] args) {
+        String students[] = { "FLÁVIA FERNANDA", "BARTOLOMEU SILVA", "REGIANE MARTINS" };
+        
+        //Forma abreviada
+        for(String student: students){
+            System.out.println(student);
+        }
+    }
+}
